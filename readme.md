3d120da147402a91290ca4991398d9cc456dd66b
adam-nasir <the.exp.nomad07@gmail.com>
2022-08-17 13:30:16 -0400

    Update 2.0

--- readme.md
+++ readme.md
@@ -1,60 +1,10 @@
-I finshed the index.scss for all scss folders
+#Personal Chef Website
 
-Make tab logo
+Its a website that I designed mockup and built it out.
 
-relearn how to extend
+##Tech Stack
 
-working on navbar
-
-https://www.youtube.com/watch?v=IxRJ8vplzAo
-
-Thing to do:
-
-Section block needs to apart of the section
-
-Header
-[] favicon
-
-Form
-[X] going to tutorial about forms and finish the form
-[X] Finish the textarea
-
-Testinomies
-[X] fix the images for the testinomies
-[X] change their color
-[X] add some spacing to the testinomies
-
-Media Qureies
-[X] Media Quries for Mobile
-[X] Story Section
-[X] Fix the title
-[X] Fix the p font-size
-[X] Inspiration Section
-[X] Fix the title
-[X] Testnomies Section
-[X] Fix the quotes text
-[X] Contact Section
-[X] Fix the label font-size
-[X] Fix the Textarea size
-
-[X] Media Quries for Mid Screens (992px)
-    [X] Nav Section
-        [X] Fix the title
-        [X] Fix the p font-size
-    [X] Hero Section
-        [X] Fix the title
-        [X] Fix the p font-size
-    [X] Story Section
-        [X] Fix the title
-        [X] Fix the p font-size
-    [X] Inspiration Section
-        [X] Fix the title
-    [X] Services Section
-        
-        [X] Find a background for this media query
-        []  
-    [] Testnomies Section
-        [] Fix the quotes text
-    [] Contact Section
-        [] Fix the label font-size
-        [] Fix the Textarea size
+- HTML
+- Dart SASS
+- JavaScript
+- GSAP
--- scripts/app.js
+++ scripts/app.js
@@ -14,6 +14,8 @@ const navbarList = document.querySelector(".navbar__list");
 
 const navbarMenu = document.querySelector(".navbar__menu");
 
+const navbarListItems = navbarList.children;
+
 console.log(navbarMenu);
 /*============================*/
 /*---- Callback functions ----*/
@@ -33,6 +35,15 @@ const openCloseMobileMenu = () => {
   navbarList.classList.toggle("appear");
 };
 
+Array.from(navbarListItems).forEach((item) => {
+  item.addEventListener("click", () => {
+    hamburgerMenu.classList.remove("open");
+    body.classList.remove("no-scroll");
+    navbarMenu.classList.remove("appearTwo");
+    navbarList.classList.remove("appear");
+  });
+});
+
 /*========================*/
 /*---- Event Listener ----*/
 /*========================*/
@@ -40,7 +51,111 @@ const openCloseMobileMenu = () => {
 window.addEventListener("scroll", headerScroll);
 hamburgerMenu.addEventListener("click", openCloseMobileMenu);
 
-// window.addEventListener("scroll", () => {
-//   console.log(this.scrollY);
-//   if
-// });
+/*==============*/
+/*---- GSAP ----*/
+/*==============*/
+
+// Header Section
+
+let headerHerotl = gsap.timeline({
+  scrollTrigger: {
+    trigger: ".main-header",
+    toggleAction: "play complete none none",
+  },
+  ease: "power4.inOut",
+});
+
+headerHerotl
+  .fromTo(".logo", { opacity: 0 }, { opacity: 1, duration: 0.2 })
+  .fromTo(".navbar", { opacity: 0 }, { opacity: 1, duration: 0.8 })
+  .fromTo(
+    ".hero__content",
+    { opacity: 0, y: 100 },
+    { opacity: 1, y: 0, duration: 0.9 }
+  );
+
+// Story Section
+
+let storytl = gsap.timeline({
+  scrollTrigger: {
+    trigger: ".about",
+
+    start: "90px 200px",
+  },
+  ease: "power4.inOut",
+});
+
+storytl
+  .fromTo(
+    ".about__img-content",
+    { opacity: 0, x: -100 },
+    { opacity: 1, x: 0, duration: 1 }
+  )
+  .fromTo(
+    ".about__text-content",
+    { opacity: 0, x: 100 },
+    { opacity: 1, x: 0, duration: 1.2 }
+  );
+
+// Inspiration Section
+
+let inspirationtl = gsap.timeline({
+  scrollTrigger: {
+    trigger: ".inspiration",
+
+    start: "100px 200px",
+  },
+  ease: "power4.inOut",
+});
+
+inspirationtl
+  .fromTo(
+    ".my-inspiration__text-content",
+    { opacity: 0, x: -100 },
+    { opacity: 1, x: 0, duration: 1 }
+  )
+  .fromTo(
+    ".inspiration__img-content",
+    { opacity: 0, x: 100 },
+    { opacity: 1, x: 0, duration: 1.5 }
+  );
+
+// Service Section
+
+let servicestl = gsap.timeline({
+  scrollTrigger: {
+    trigger: ".services",
+
+    start: "60px 200px",
+  },
+  ease: "power4.inOut",
+});
+
+servicestl.from(".card", { stagger: 0.3, opacity: 0, y: -100 });
+
+// Testimonies Section
+
+let testimoniestl = gsap.timeline({
+  scrollTrigger: {
+    trigger: ".testimonies",
+
+    start: "60px 200px",
+  },
+  ease: "power4.inOut",
+});
+
+testimoniestl
+  .from(".testimonies .section-title", { opacity: 0, duration: 0.5 })
+  .from(".testimonies__content", { opacity: 0 }, "+=0.2");
+
+// Contact Section
+
+let contacttl = gsap.timeline({
+  scrollTrigger: {
+    trigger: ".contact",
+    start: "60px 200px",
+  },
+  ease: "power4.inOut",
+});
+
+contacttl.from(".form", { opacity: 0, y: 40, duration: 1.2 });
